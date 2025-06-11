2cb3f67a0dc65bbc7995190707571dd890d28638
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-06-11 19:55:09 +0300

    route handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
 const app = express();
-app.use((req, res) => {
-  res.send("Free Palestine!");
+
+app.get("/users", (req, res) => {
+  res.send("here the users data yu requested");
 });
-app.listen(3000, () => {
+
+app.post("/users", (req, res) => {
+  console.log("processing the request...");
+  console.log("validating the request data...");
+  console.log("saving data to DB");
+
+  res.send("user created successfully");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
   console.log("server is running on port 3000");
 });
