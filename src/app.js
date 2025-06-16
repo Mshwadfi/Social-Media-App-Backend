1de13772bc85577b293d0813b7e5cf5f54a504c3
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-06-16 15:33:40 +0300

    error handling

--- src/app.js
+++ src/app.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const app = express();
-
-app.use("/users", userAuth, (req, res, next) => {
-  next();
-});
-app.get(
-  "/users",
-
-  (req, res) => {
-    res.send("users fetched successfully!");
-  }
-);
-
-app.post("/users", (req, res) => {
-  res.send("user added to data base");
-});
-
-app.patch("/users", (req, res) => {
-  res.send("user data updated successfully");
+// always use try catch
+// app.use("/", (err, req, res, next) => {
+//   if (err) {
+//     res.status("500").send("some thing went wrong");
+//   }
+// });
+app.get("/users", (req, res) => {
+  // try {
+  // res.send("users fetched successfully!");
+  throw new Error("an error happend");
+  res.send("hello");
+  // } catch (error) {
+  // res.status(500).send("some thing went wrong from get");
+  // next(err);
+  // }
 });
 
-app.delete("/users", (req, res) => {
-  res.send("user has been deleted successfully");
-});
+// app.use("/", (err, req, res, next) => {
+//   if (err) {
+//     res.status(500).send("some thing went wrong");
+//   }
+// });
 
-app.get("/admin", adminAuth, (req, res) => {
-  res.send(" admin data fetched successfully!");
-});
 app.listen(4000, () => {
   console.log("server is running on prot 4000");
 });
