feda5fcc6e3c5d15da8bdcbd134e5cbc8440976e
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-06-18 12:25:06 +0300

    full user crud apis

--- src/app.js
+++ src/app.js
@@ -3,18 +3,81 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+// sign up user
 app.post("/signUp", async (req, res) => {
-  const userObj = {
-    firstName: "Muhammad",
-    lastName: "Alshwadfy",
-    email: "mohamedalshwadfy24@gmail.com",
-    gender: "male",
-    age: "24",
-  };
-
-  const user = new User(userObj);
+  console.log(req.body);
+  const user = new User(req.body);
   await user.save(user);
   res.send("user created successfully!");
+});
+
+// get user by id
+app.get("/users/:id", async (req, res) => {
+  const userId = req.params.id;
+  console.log(req.params);
+
+  try {
+    const user = await User.findById(userId);
+    if (user.length !== 0) {
+      res.status(200).send(user);
+    } else {
+      res.status(404).send("user not found");
+    }
+  } catch (error) {
+    res.send("something went wrong");
+  }
+});
+//get All users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    console.log(users);
+    if (users.length === 0) {
+      res.status(200).send("there is no users");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (error) {
+    res.status(404).send("not found");
+  }
+});
+
+// update user
+app.patch("/users/:id", async (req, res) => {
+  const userId = req.params.id;
+  const data = req.body;
+  console.log(req.body, req.params.id);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
+      new: true,
+    });
+    if (updatedUser) {
+      res.status(200).send({
+        message: "user updated successfully",
+        user: updatedUser,
+      });
+    } else {
+      res.status(404).send("user not found");
+    }
+  } catch (error) {
+    res.status(500).send("internal server error");
+  }
+});
+
+// delete user by id
+app.delete("/users/:id", async (req, res) => {
+  const userId = req.params.id;
+  console.log(req.body);
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(500).send("internal server error");
+  }
 });
 
 connectDB()
