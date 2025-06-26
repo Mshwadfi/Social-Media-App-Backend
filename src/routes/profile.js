0bbe4cfa3d51c7dad7e3d9e89e9ba1e3a9b30a12
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-06-26 12:24:12 +0300

    creating get users profile api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const User = require("../models/user");
 const { authMiddleWare } = require("../middlewares/auth");
+const { default: mongoose } = require("mongoose");
 
 const profileRouter = express.Router();
 
-//get profile api
+//get loggedin user profile api
 profileRouter.get("/profile", authMiddleWare, async (req, res) => {
   try {
     const user = await User.findById(req.user._id);
@@ -21,6 +22,39 @@ profileRouter.get("/profile", authMiddleWare, async (req, res) => {
     });
   } catch (error) {
     console.error(error.message);
+    res.status(500).send({ success: false, error: "internal server error" });
+  }
+});
+
+// get other users Profile
+
+profileRouter.get("/profile/:id", authMiddleWare, async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({
+        success: false,
+        error: "Invalid user ID",
+      });
+    }
+    const user = await User.findById(id).select([
+      "firstName",
+      "lastName",
+      "age",
+      "gender",
+    ]);
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        error: "User not found",
+      });
+    }
+    res.send({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
     res.status(500).send({ success: false, error: "internal server error" });
   }
 });
