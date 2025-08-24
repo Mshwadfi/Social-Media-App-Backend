acf417049ca013edde54239beeb282ecf8c8d9b6
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-08-24 14:56:48 +0300

    implement cach-aside strategy for profiles data with redis

---
+++ src/config/redis.js
@@ -0,0 +1,17 @@
+const Redis = require("ioredis");
+
+// Defaults to localhost:6379
+const redis = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+redis.on("connect", () => {
+  console.log("✅ Redis connected");
+});
+
+redis.on("error", (err) => {
+  console.error("❌ Redis error:", err);
+});
+
+module.exports = redis;
--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,12 +1,25 @@
+const { default: Redis } = require("ioredis");
 const { AppError, asyncHandler } = require("../middlewares/errorHandler");
 const User = require("../models/user");
 const mongoose = require("mongoose");
+const redis = require("../config/redis");
 
 const allowedFields = ["firstName", "lastName", "age", "gender"];
 
 exports.getLoggedInUserProfile = asyncHandler(async (req, res, next) => {
+  // check redis first
+  const cachKey = `user:${req.user._id}:profile`;
+  const cachedProfile = await redis.get(cachKey);
+  if (cachedProfile) {
+    console.log("Serving from Redis cache");
+    return res.json({ success: true, data: JSON.parse(cachedProfile) });
+  }
+
+  // if not in cache, fetch from DB
   const user = await User.findById(req.user._id);
   if (!user) return next(new AppError("User not found", 404));
+  // cache the profile data in Redis for future requests
+  await redis.set(cachKey, JSON.stringify(user), "EX", 3600);
   res.json({ success: true, data: user });
 });
 
@@ -15,9 +28,19 @@ exports.getOtherUserProfile = asyncHandler(async (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     return next(new AppError("Invalid user ID", 400));
 
+  // check redis first
+  const cachKey = `user:${id}:profile`;
+  const cachedProfile = await redis.get(cachKey);
+  if (cachedProfile) {
+    console.log("Serving from Redis cache");
+    return res.json({ success: true, data: JSON.parse(cachedProfile) });
+  }
+
+  // if not in cache, fetch from DB
   const user = await User.findById(id).select(allowedFields);
   if (!user) return next(new AppError("User not found", 404));
-
+  await redis.set(cachKey, JSON.stringify(user), "EX", 3600);
+  console.log("Serving from MongoDB");
   res.json({ success: true, data: user });
 });
 
@@ -29,7 +52,9 @@ exports.updateUserProfile = asyncHandler(async (req, res, next) => {
   }).select(allowedFields);
 
   if (!updatedUser) return next(new AppError("User not found", 404));
-
+  // Invalidate the cached profile in Redis
+  const cachKey = `user:${req.user._id}:profile`;
+  await redis.del(cachKey);
   res.json({
     success: true,
     user: updatedUser,
