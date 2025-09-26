92c79f955e197549fe533a39c47f005799221373
Mshwadfi <mohamedalshwadfy24@gmail.com>
2025-09-26 13:59:35 +0300

    handling errors in auth middleware by global error handling middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,31 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const { AppError } = require("./errorHandler");
 require("dotenv").config();
 
 const authMiddleWare = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      return res.status(401).send("unothorized");
+      return next(new AppError("Unauthorized: No token provided", 401));
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const { _id } = decoded;
     const user = await User.findById(_id);
-    if (!user) return res.status(401).send("invalid Token");
+
+    if (!user) return next(new AppError("Unauthorized: Invalid token", 401));
     req.user = user;
     next();
   } catch (error) {
-    console.error("Auth Middleware Error:", error.message);
-    return res.status(500).json({ error: "Internal Server Error" });
+    if (error.name === "JsonWebTokenError") {
+      return next(new AppError("Invalid token", 401));
+    }
+    if (error.name === "TokenExpiredError") {
+      return next(new AppError("Token expired", 401));
+    }
+
+    next(error);
   }
 };
 
